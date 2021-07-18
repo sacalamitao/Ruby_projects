69bfed12f6608e1a98020004b150f3f3003641ac
Edwin Sacalamitao <edwin@avionschool.com>
2021-07-19 03:30:19 +0800

    added some validation for tes

--- blog/app/models/loan.rb
+++ blog/app/models/loan.rb
@@ -1,4 +1,6 @@
 class Loan < ApplicationRecord
     validates :first_name, length: { in: 5..20 }
     validates :age, inclusion: 18...50
+    validates :first_name, :last_name, :age, :code, :amount, :presence => true
+    validates :code, :uniqueness => true
 end
