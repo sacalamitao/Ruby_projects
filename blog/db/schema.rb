451bc62cf743724aeec1df770d310cce63048d41
Edwin <sacalamitao.edwin@yahoo.com>
2021-07-19 20:00:51 +0800

    make articles

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,17 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_17_021607) do
+ActiveRecord::Schema.define(version: 2021_07_19_115601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "loans", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.integer "age"
-    t.integer "amount"
-    t.integer "code"
+  create_table "articles", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
