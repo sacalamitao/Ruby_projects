44f2b574e01511cda6cc2dbec6c6498560570b38
Edwin Sacalamitao <edwin@avionschool.com>
2021-07-19 20:15:19 +0800

    article fixed

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+    validates :name, length: { in: 5..20 }
+    validates :name, :body, :presence => true
 end
--- blog/app/models/loan.rb
+++
@@ -1,6 +0,0 @@
-class Loan < ApplicationRecord
-    validates :first_name, length: { in: 5..20 }
-    validates :age, inclusion: 18...50
-    validates :first_name, :last_name, :age, :code, :amount, :presence => true
-    validates :code, :uniqueness => true
-end
