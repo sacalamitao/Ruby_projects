65f640e84407b2ef24ba9f6c690631194fc799ec
Edwin Sacalamitao <edwin@avionschool.com>
2021-07-21 02:13:17 +0800

    fixed scaffold

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -22,4 +22,14 @@ ActiveRecord::Schema.define(version: 2021_07_19_115601) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "loans", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.integer "amount"
+    t.integer "code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
