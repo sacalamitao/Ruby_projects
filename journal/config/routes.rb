0a70654b197f5c66ca7e8965178d09fa29cb51b7
Edwin Sacalamitao <edwin@avionschool.com>
2021-07-30 23:21:35 +0800

    edit controller created, template error

--- journal/config/routes.rb
+++ journal/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :task_organizations
-  root to: 'task_organizations#create'
+  root to: 'task_organizations#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
