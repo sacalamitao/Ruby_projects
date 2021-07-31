5d479a9a69316329b3ceeee22917c8d4cfb4ac48
Edwin Sacalamitao <edwin@avionschool.com>
2021-07-31 08:54:28 +0800

    created default model using before all

--- journal/spec/system/creating_task_organizations_spec.rb
+++ journal/spec/system/creating_task_organizations_spec.rb
@@ -8,17 +8,15 @@ RSpec.describe "CreatingTaskOrganizations", type: :system do
   # pending "add some scenarios (or delete) #{__FILE__}"
   it 'saves and displays the resulting task organization' do
     visit '/task_organizations/new'
-
     fill_in 'Title', with: 'Hello, World!'
     fill_in 'Body', with: 'Hello, I say!'
-    click_on 'Create Task Organizations'
+    click_on 'Submit Task Organizations'
 
     expect(page).to have_content("Hello, World!")
     expect(page).to have_content("Hello, I say!")
 
-    task_organizations = TaskOrganization.order("id").last
-    expect(task_organizations.title).to eq('Hello, World!')
-    expect(task_organizations.body).to eq('Hello, I say!')
+    task_organization = TaskOrganization.order("id").last
+    expect(task_organization.title).to eq('Hello, World!')
+    expect(task_organization.body).to eq('Hello, I say!')
   end
-
 end
